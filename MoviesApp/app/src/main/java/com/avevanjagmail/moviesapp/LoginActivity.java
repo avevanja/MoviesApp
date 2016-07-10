b2a0c88bb6e2b55f5f473bb501f768dc34ff1c46
John Taylor <John Taylor>
2016-07-10 15:12:55 +0300

    Tab and MainActivity

--- MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/LoginActivity.java
+++ MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/LoginActivity.java
@@ -4,26 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity  {
     TextView tvReg;
+    Button btnLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         tvReg = (TextView) findViewById(R.id.tV_reg);
-        tvReg.setOnClickListener(this);
+
+        btnLog = (Button) findViewById(R.id.btn_log);
+
 
     }
 
-    @Override
+
     public void onClick(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, RegistrationActivity.class);
         startActivity(intent);
 
 
         }
+    public void onClick1(View view) {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
+
+
+    }
 
     }
