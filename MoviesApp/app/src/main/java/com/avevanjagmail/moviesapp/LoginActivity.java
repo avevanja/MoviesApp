d1655e17d3ca8aea81d36ae0d609beefef2b7079
John Taylor <John Taylor>
2016-07-10 11:17:21 +0300

    Revert "Revert "Registration Activity""
    This reverts commit eb60fdea347f50772a79b8c0dbca683d15da3096.

--- MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/LoginActivity.java
+++ MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.avevanjagmail.moviesapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView tvReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvReg = (TextView) findViewById(R.id.tV_reg);
+        tvReg.setOnClickListener(this);
+
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+
+
+        }
+
+    }
