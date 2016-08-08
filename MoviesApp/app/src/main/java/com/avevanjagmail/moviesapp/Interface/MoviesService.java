7876bf4b9be7e8fb097b1fa494c4d85b9e311fcb
Pavlo Grynyk <pavlo.grynyk@indeema.com>
2016-08-08 16:27:45 +0300

    some new Inform

--- MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/Interface/MoviesService.java
+++ MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/Interface/MoviesService.java
@@ -1,16 +1,21 @@
 package com.avevanjagmail.moviesapp.Interface;
 
+import com.avevanjagmail.moviesapp.Models.MoviesInfo;
 import com.avevanjagmail.moviesapp.Models.ListMovie;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
  * Created by John on 15.07.2016.
  */
 public interface MoviesService {
-    String URL = "http://api.themoviedb.org";
+
+    String URL= "http://api.themoviedb.org";
+
+
 
     @GET("3/movie/top_rated/?api_key=a143b2488bf72e7081edb871e0db3a7c")
     Call<ListMovie> getTopName(@Query("language") String lg, @Query("page") int page);
@@ -20,6 +25,10 @@ public interface MoviesService {
 
     @GET("3/movie/now_playing/?api_key=a143b2488bf72e7081edb871e0db3a7c")
     Call<ListMovie> getMovieInfo(@Query("language") String lg, @Query("page") int page);
+
+    @GET("3/movie/{id}?api_key=a143b2488bf72e7081edb871e0db3a7c")
+    Call<MoviesInfo> getMovieInfoFromId(@Path("id")String idMovie, @Query("language") String lg);
+
 
 
 
--- MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/activities/InformActivity.java
+++ MoviesApp/app/src/main/java/com/avevanjagmail/moviesapp/activities/InformActivity.java
@@ -3,23 +3,39 @@ package com.avevanjagmail.moviesapp.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.avevanjagmail.moviesapp.Interface.MoviesService;
+import com.avevanjagmail.moviesapp.Models.Genre;
+import com.avevanjagmail.moviesapp.Models.MoviesInfo;
 import com.avevanjagmail.moviesapp.R;
+import com.avevanjagmail.moviesapp.utils.RetrofitUtil;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by paulg on 03.08.2016.
  */
 public class InformActivity extends AppCompatActivity {
-    private TextView TextTet;
+    private static final String TAG = "InformActivity" ;
+    private TextView TextTet, mOverviewTextView;
     private ImageView TitleImageView;
     private Toolbar toolbarInformActivity;
+    private ArrayList<Genre> mListMovie;
+
+
 
 
     private static final String MOVIE_ID = "movie.id";
@@ -52,9 +68,42 @@ public class InformActivity extends AppCompatActivity {
         setTitle(getIntent().getStringExtra(TITLE));
         TitleImageView = (ImageView)findViewById(R.id.expandedImage);
         TextTet = (TextView)findViewById(R.id.inform_movie_text);
+        mOverviewTextView = (TextView)findViewById(R.id.text_overview);
         String text = getIntent().getStringExtra(MOVIE_ID);
-        TextTet.setText(text.toString());
+//        TextTet.setText(text.toString());
         Picasso.with(this).load("https://image.tmdb.org/t/p/w533_and_h300_bestv2" + getIntent().getStringExtra(URL_Image)).
                 error(R.drawable.ava).resize(717, 400).into(TitleImageView);
+        Gson gson = new GsonBuilder().create();
+
+        MoviesService mService = RetrofitUtil.getMoviesService();
+        Call<MoviesInfo> requestMovie = mService.getMovieInfoFromId(text, "ru");
+        requestMovie.enqueue(getCallback());
+//        TextTet.setText("bla");
+    }
+    private Callback<MoviesInfo> getCallback() {
+        Log.d(TAG, "getCallback");
+        return new Callback<MoviesInfo>() {
+            @Override
+            public void onResponse(Call<MoviesInfo> call, Response<MoviesInfo> response) {
+                Log.d(TAG, "getCallback onResponse");
+                mListMovie = (ArrayList<Genre>) response.body().getGenres();
+                for (Genre genre : mListMovie)
+                {
+                    TextTet.setText(genre.getName() + " ");
+                }
+                mOverviewTextView.setText(response.body().getOverview());
+
+//                TextTet.setText(mGenre.getName());
+//                TextTet.setText("bla");
+
+
+            }
+
+            @Override
+            public void onFailure(Call<MoviesInfo> call, Throwable t) {
+                Log.e(TAG, "Eror" + t.getMessage());
+                t.printStackTrace();
+            }
+        };
     }
 }
